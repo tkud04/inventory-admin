7e863f546a6e695ebb54f4f11ecc718f39bb0b59
ANonymous <postmaster.richmama@gmail.com>
2020-02-03 11:57:56 +0100

    added input tmb

--- resources/views/payment-callback.blade.php
+++ resources/views/payment-callback.blade.php
@@ -2,9 +2,10 @@
       <head></head>
       <body>
       	<p>Payment successful! Redirecting you to Tembo..</p>
+		<input type="hidden" id="tmb" value="{{$dt}}"/>
         <script>
           setTimeout(function () {
-            window.ReactNativeWebView.postMessage("{{$dt}}");
+            window.ReactNativeWebView.postMessage(document.querySelector('#tmb').value);
           }, 2000)
         </script>
       </body>
